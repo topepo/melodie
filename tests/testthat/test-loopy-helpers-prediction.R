c5b09b14299ce20216de28cc2d73b9b2174bc112
topepo <mxkuhn@gmail.com>
2025-04-09 14:57:40 -0400

    tests for regression with submodels

--- tests/testthat/test-loopy-helpers-prediction.R
+++ tests/testthat/test-loopy-helpers-prediction.R
@@ -44,6 +44,7 @@ test_that("determinig prediction/post strategy", {
 
 test_that("prediction only, no submodels, classification", {
   skip_if_not_installed("modeldata")
+  skip_if_not_installed("C50")
 
   data("two_class_dat", package = "modeldata")
   two_class_rs <- mc_cv(two_class_dat, times = 2)
@@ -216,3 +217,189 @@ test_that("prediction only, no submodels, regression", {
 
 })
 
+
+test_that("prediction only, with submodels, classification", {
+  skip_if_not_installed("modeldata")
+  skip_if_not_installed("kknn")
+
+  data("two_class_dat", package = "modeldata")
+  two_class_rs <- mc_cv(two_class_dat, times = 2)
+  rs_split <- two_class_rs$splits[[1]]
+  mc_cv_args <- rsample::.get_split_args(two_class_rs)
+
+  wflow <-  workflow(Class ~ ., knn_cls_spec)
+  knn_grid <- tibble(neighbors = 1:3)
+
+  wflow_fit <-
+    wflow %>%
+    finalize_workflow(knn_grid[1,]) %>%
+    .fit_pre(data = analysis(rs_split)) %>%
+    .fit_model(control = control_workflow())
+
+  # ------------------------------------------------------------------------------
+  # class and prob predictions
+
+  static_1 <- melodie:::make_static(
+    wflow,
+    param_info = wflow %>% extract_parameter_set_dials(),
+    metrics = metric_set(accuracy, brier_class),
+    eval_time = NULL,
+    split_args = mc_cv_args,
+    control = control_resamples()
+  )
+
+  data_1 <- melodie:::get_data_subsets(wflow, rs_split)
+  static_1 <- c(static_1, data_1)
+  static_1$y_name <- "Class"
+
+  sched <- melodie:::schedule_grid(knn_grid, static_1$wflow)
+
+  res_1 <-
+    melodie:::predict_only(
+      wflow_current = wflow_fit,
+      sched = sched$model_stage[[1]][1,],
+      grid = knn_grid[1,],
+      static = static_1,
+      estimation = FALSE
+    )
+  plist_1 <-
+    tibble(
+      Class = two_class_dat$Class[0],
+      .pred_class = two_class_dat$Class[0],
+      .pred_Class1 = double(0),
+      .pred_Class2 = double(0),
+      .row = integer(0),
+      neighbors = double(0)
+    )
+  expect_equal(res_1[0,], plist_1)
+  expect_equal(nrow(res_1), nrow(assessment(rs_split)) * nrow(knn_grid))
+  expect_equal(
+    res_1$.row,
+    rep(as.integer(rs_split, data = "assessment"), each = nrow(knn_grid))
+  )
+  expect_equal(sort(unique(res_1$neighbors)), sort(unique(knn_grid$neighbors)))
+
+  # ------------------------------------------------------------------------------
+  # class predictions only
+
+  acc_mtr <- metric_set(accuracy)
+  static_2 <- static_1
+  static_2$metrics <- acc_mtr
+  static_2$metric_info <- tibble::as_tibble(acc_mtr)
+  static_2$pred_types <- unique(metrics_info(acc_mtr)$type)
+
+  res_2 <-
+    melodie:::predict_only(
+      wflow_current = wflow_fit,
+      sched = sched$model_stage[[1]][1,],
+      grid = knn_grid[1,],
+      static = static_2,
+      estimation = FALSE
+    )
+  plist_2 <-
+    tibble(
+      Class = two_class_dat$Class[0],
+      .pred_class = two_class_dat$Class[0],
+      .row = integer(0),
+      neighbors = double(0)
+    )
+  expect_equal(res_2[0,], plist_2)
+  expect_equal(nrow(res_2), nrow(assessment(rs_split)) * nrow(knn_grid))
+  expect_equal(
+    res_2$.row,
+    rep(as.integer(rs_split, data = "assessment"), each = nrow(knn_grid))
+  )
+  expect_equal(sort(unique(res_2$neighbors)), sort(unique(knn_grid$neighbors)))
+
+  # ------------------------------------------------------------------------------
+  # class probabilities only
+
+  brier_mtr <- metric_set(brier_class)
+  static_3 <- static_1
+  static_3$metrics <- brier_mtr
+  static_3$metric_info <- tibble::as_tibble(brier_mtr)
+  static_3$pred_types <- unique(metrics_info(brier_mtr)$type)
+
+  res_3 <-
+    melodie:::predict_only(
+      wflow_current = wflow_fit,
+      sched = sched$model_stage[[1]][1,],
+      grid = knn_grid[1,],
+      static = static_3,
+      estimation = FALSE
+    )
+  plist_3 <-
+    tibble(
+      Class = two_class_dat$Class[0],
+      .pred_Class1 = double(0),
+      .pred_Class2 = double(0),
+      .row = integer(0),
+      neighbors = double(0)
+    )
+  expect_equal(res_3[0,], plist_3)
+  expect_equal(nrow(res_3), nrow(assessment(rs_split)) * nrow(knn_grid))
+  expect_equal(
+    res_3$.row,
+    rep(as.integer(rs_split, data = "assessment"), each = nrow(knn_grid))
+  )
+  expect_equal(sort(unique(res_3$neighbors)), sort(unique(knn_grid$neighbors)))
+})
+
+
+test_that("prediction only, with submodels, regression", {
+  skip_if_not_installed("kknn")
+
+  reg_rs <- mc_cv(puromycin, times = 2)
+  rs_split <- reg_rs$splits[[1]]
+  mc_cv_args <- rsample::.get_split_args(reg_rs)
+
+  wflow <-  workflow(rate ~ ., knn_reg_spec)
+  knn_grid <- tibble(neighbors = 1:3)
+
+  wflow_fit <-
+    wflow %>%
+    finalize_workflow(knn_grid[1,]) %>%
+    .fit_pre(data = analysis(rs_split)) %>%
+    .fit_model(control = control_workflow())
+
+  # ------------------------------------------------------------------------------
+
+  static_1 <- melodie:::make_static(
+    wflow,
+    param_info = wflow %>% extract_parameter_set_dials(),
+    metrics = metric_set(rmse),
+    eval_time = NULL,
+    split_args = mc_cv_args,
+    control = control_resamples()
+  )
+
+  data_1 <- melodie:::get_data_subsets(wflow, rs_split)
+  static_1 <- c(static_1, data_1)
+  static_1$y_name <- "rate"
+
+  sched <- melodie:::schedule_grid(knn_grid, static_1$wflow)
+
+  res_1 <-
+    melodie:::predict_only(
+      wflow_current = wflow_fit,
+      sched = sched$model_stage[[1]][1,],
+      grid = knn_grid[1,],
+      static = static_1,
+      estimation = FALSE
+    )
+  plist_1 <-
+    tibble(
+      rate = puromycin$rate[0],
+      .pred = puromycin$rate[0],
+      .row = integer(0),
+      neighbors = double(0)
+    )
+  expect_equal(res_1[0,], plist_1)
+  expect_equal(nrow(res_1), nrow(assessment(rs_split)) * nrow(knn_grid))
+  expect_equal(
+    res_1$.row,
+    rep(as.integer(rs_split, data = "assessment"), each = nrow(knn_grid))
+  )
+  expect_equal(sort(unique(res_1$neighbors)), sort(unique(knn_grid$neighbors)))
+
+})
