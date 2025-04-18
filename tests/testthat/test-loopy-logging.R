a8d71030810f0b3b7be95382849b1080468f35f0
Emil Hvitfeldt <emil.hvitfeldt@posit.co>
2025-04-18 09:51:15 -0700

    Add notes in loopy (#3)
    * add .notes in loopy
    
    * update logging tests to verify .notes
    
    * make .catch_and_log() work with warnings
    
    * test that .catch_and_log() works with errors, warnings and messages
    
    * tune_grid -> melodie_grid
    
    ---------
    
    Co-authored-by: topepo <mxkuhn@gmail.com>

--- tests/testthat/test-loopy-logging.R
+++ tests/testthat/test-loopy-logging.R
@@ -9,8 +9,7 @@ test_that("preprocessor error doesn't stop grid", {
   exp <- bind_cols(
     folds,
     tibble(
-      .metrics = list(NULL),
-      .notes = list(tibble())
+      .metrics = list(NULL)
     )
   )
 
@@ -23,10 +22,14 @@ test_that("preprocessor error doesn't stop grid", {
   )
 
   expect_identical(
-    res_fit, 
+    dplyr::select(res_fit, -.notes),
     exp,
     ignore_attr = TRUE
   )
+
+  expect_identical(nrow(res_fit$.notes[[1]]), 1L)
+  expect_identical(ncol(res_fit$.notes[[1]]), 3L)
+  expect_true(all(vapply(res_fit$.notes[[1]], is.character, logical(1))))
 })
 
 test_that("model error doesn't stop grid", {
@@ -40,8 +43,7 @@ test_that("model error doesn't stop grid", {
   exp <- bind_cols(
     folds,
     tibble(
-      .metrics = list(NULL),
-      .notes = list(tibble())
+      .metrics = list(NULL)
     )
   )
 
@@ -49,7 +51,7 @@ test_that("model error doesn't stop grid", {
   mod_spec <- parsnip::nearest_neighbor("regression", "kknn", dist_power = tune())
 
   wf_spec <- workflow(rec_spec, mod_spec)
-  
+
   res_fit <- melodie_grid(
     wf_spec,
     folds,
@@ -58,10 +60,14 @@ test_that("model error doesn't stop grid", {
   )
 
   expect_identical(
-    res_fit, 
+    dplyr::select(res_fit, -.notes),
     exp,
     ignore_attr = TRUE
   )
+
+  expect_identical(nrow(res_fit$.notes[[1]]), 2L)
+  expect_identical(ncol(res_fit$.notes[[1]]), 3L)
+  expect_true(all(vapply(res_fit$.notes[[1]], is.character, logical(1))))
 })
 
 test_that("prediction error doesn't stop grid", {
@@ -78,8 +84,7 @@ test_that("prediction error doesn't stop grid", {
   exp <- bind_cols(
     folds,
     tibble(
-      .metrics = list(NULL),
-      .notes = list(tibble())
+      .metrics = list(NULL)
     )
   )
 
@@ -87,7 +92,7 @@ test_that("prediction error doesn't stop grid", {
   mod_spec <- parsnip::nearest_neighbor("regression", "kknn", dist_power = tune())
 
   wf_spec <- workflow(rec_spec, mod_spec)
-  
+
   res_fit <- melodie_grid(
     wf_spec,
     folds,
@@ -96,8 +101,136 @@ test_that("prediction error doesn't stop grid", {
   )
 
   expect_identical(
-    res_fit, 
+    dplyr::select(res_fit, -.notes),
     exp,
     ignore_attr = TRUE
   )
+
+  expect_identical(nrow(res_fit$.notes[[1]]), 2L)
+  expect_identical(ncol(res_fit$.notes[[1]]), 3L)
+  expect_true(all(vapply(res_fit$.notes[[1]], is.character, logical(1))))
+})
+
+test_that("capturing error correctly in notes", {
+  assign("prep.step_logging_helper", prep.step_logging_helper, envir = .GlobalEnv)
+  assign("bake.step_logging_helper", bake.step_logging_helper, envir = .GlobalEnv)
+  ames <- modeldata::ames[, c(72, 40:45)]
+
+  set.seed(1234)
+  folds <- rsample::vfold_cv(ames, 2)
+
+  rec_spec <- recipe(Sale_Price ~ ., ames) %>%
+    step_logging_helper(type = "error")
+  mod_spec <- parsnip::nearest_neighbor(
+    "regression",
+    "kknn",
+    dist_power = tune()
+  )
+
+  wf_spec <- workflow(rec_spec, mod_spec)
+
+  res_fit <- melodie_grid(
+    wf_spec,
+    folds,
+    grid = 2,
+    control = control_grid(allow_par = FALSE)
+  )
+
+  exp <- tibble::tibble(
+    location = "preprocessor 1/1",
+    type = "error",
+    note = "testing error"
+  )
+  expect_identical(res_fit$.notes[[1]], exp)
+  expect_identical(res_fit$.notes[[2]], exp)
+
+  expect_true(
+    all(vapply(res_fit$.metrics, NROW, integer(1)) == 0)
+  )
+  rm(list = "prep.step_logging_helper", envir = .GlobalEnv)
+  rm(list = "bake.step_logging_helper", envir = .GlobalEnv)
+})
+
+test_that("capturing warning correctly in notes", {
+  assign("prep.step_logging_helper", prep.step_logging_helper, envir = .GlobalEnv)
+  assign("bake.step_logging_helper", bake.step_logging_helper, envir = .GlobalEnv)
+
+  ames <- modeldata::ames[, c(72, 40:45)]
+
+  set.seed(1234)
+  folds <- rsample::vfold_cv(ames, 2)
+
+  rec_spec <- recipe(Sale_Price ~ ., ames) %>%
+    step_logging_helper(type = "warning")
+  mod_spec <- parsnip::nearest_neighbor(
+    "regression",
+    "kknn",
+    dist_power = tune()
+  )
+
+  wf_spec <- workflow(rec_spec, mod_spec)
+  res_fit <- melodie_grid(
+    wf_spec,
+    folds,
+    grid = 2,
+    control = control_grid(allow_par = FALSE)
+  )
+
+  exp <- tibble::tibble(
+    location = "preprocessor 1/1",
+    type = "warning",
+    note = "testing warning"
+  )
+  expect_identical(res_fit$.notes[[1]], exp)
+  expect_identical(res_fit$.notes[[2]], exp)
+
+  expect_true(
+    all(vapply(res_fit$.metrics, NROW, integer(1)) > 0)
+  )
+  rm(list = "prep.step_logging_helper", envir = .GlobalEnv)
+  rm(list = "bake.step_logging_helper", envir = .GlobalEnv)
+})
+
+test_that("doesn't capturing message in notes", {
+  assign("prep.step_logging_helper", prep.step_logging_helper, envir = .GlobalEnv)
+  assign("bake.step_logging_helper", bake.step_logging_helper, envir = .GlobalEnv)
+
+  ames <- modeldata::ames[, c(72, 40:45)]
+
+  set.seed(1234)
+  folds <- rsample::vfold_cv(ames, 2)
+
+  rec_spec <- recipe(Sale_Price ~ ., ames) %>%
+    step_logging_helper(type = "message")
+  mod_spec <- parsnip::nearest_neighbor(
+    "regression",
+    "kknn",
+    dist_power = tune()
+  )
+
+  wf_spec <- workflow(rec_spec, mod_spec)
+
+  expect_snapshot(
+    res_fit <- melodie_grid(
+      wf_spec,
+      folds,
+      grid = 2,
+      control = control_grid(allow_par = FALSE)
+    )
+  )
+
+  exp <- tibble::tibble(
+    location = character(0),
+    type = character(0),
+    note = character(0)
+  )
+
+  expect_identical(res_fit$.notes[[1]], exp)
+  expect_identical(res_fit$.notes[[2]], exp)
+
+  expect_true(
+    all(vapply(res_fit$.metrics, NROW, integer(1)) > 0)
+  )
+  rm(list = "prep.step_logging_helper", envir = .GlobalEnv)
+  rm(list = "bake.step_logging_helper", envir = .GlobalEnv)
 })
